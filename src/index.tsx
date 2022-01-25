b6a71b1dab79da048d03679601a177623af69487
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-25 17:10:17 -0500

    Add Firebase SDK

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjdihUlVyjK-ufua178sz1hQFkYO1cD7k",
+  authDomain: "speed-friending.firebaseapp.com",
+  projectId: "speed-friending",
+  storageBucket: "speed-friending.appspot.com",
+  messagingSenderId: "978700228245",
+  appId: "1:978700228245:web:3a5d37c6a57e99e0cfad9c",
+  measurementId: "G-9N2TFFNNEF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
