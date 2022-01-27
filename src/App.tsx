b81056b976f0a45c49158ee4cd7537706b9d790c
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-27 17:27:03 -0500

    Add display styles and clean up algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,94 @@ import React from "react";
 
 import getGame from "./getGame";
 
+const getColor = (n: number) => {
+  if (n === 0) {
+    return "#a9a9a9"; // Grey
+  } else if (n % 10 === 0) {
+    return "#ffffff"; // Black
+  } else if (n % 9 === 0) {
+    return "#f032e6"; // Magenta
+  } else if (n % 8 === 0) {
+    return "#911eb4"; // Purple
+  } else if (n % 7 === 0) {
+    return "#4363d8"; // Blue
+  } else if (n % 6 === 0) {
+    return "#42d4f4"; // Cyan
+  } else if (n % 5 === 0) {
+    return "#3cb44b"; // Green
+  } else if (n % 4 === 0) {
+    return "#bfef45"; // Lime
+  } else if (n % 3 === 0) {
+    return "#ffe119"; // Yellow
+  } else if (n % 2 === 0) {
+    return "#f58231"; // Orange
+  } else if (n % 1 === 0) {
+    return "#e6194B"; // Red
+  } else {
+    return "#a9a9a9"; // Grey
+  }
+};
+
 const App = () => {
-  const game = getGame(9);
+  const game = getGame(4, 10);
 
   return (
     <div>
       {Object.values(game).map((round, index) => {
         return (
-          <div>
+          <div style={{ margin: "20px" }} key={index}>
             <h3>{`Round ${index + 1}`}</h3>
-            {round.top.map((n) => (
-              <span>{`[${n}]`}</span>
-            ))}
-            <br />
-            {round.btm.map((n) => (
-              <span>{`[${n}]`}</span>
-            ))}
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: `repeat(${round.top.length}, 1fr)`,
+                alignItems: "center",
+                justifyItems: "center",
+              }}
+            >
+              {round.top.map((n, topIdx) => (
+                <div
+                  key={topIdx}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "60px",
+                    width: "60px",
+                    color: "black",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    backgroundColor: getColor(n),
+                    border: "2px solid black",
+                    boxSizing: "border-box",
+                  }}
+                >
+                  {n}
+                </div>
+              ))}
+              {round.btm.map((n, btmIdx) => (
+                <div
+                  key={btmIdx}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "60px",
+                    width: "60px",
+                    color: "black",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    backgroundColor: getColor(n),
+                    border: "2px solid black",
+                    boxSizing: "border-box",
+                  }}
+                >
+                  {n}
+                </div>
+              ))}
+            </div>
           </div>
         );
       })}
--- src/getGame.tsx
+++ src/getGame.tsx
@@ -2,26 +2,25 @@ type GameType = {
   [x: number]: { top: Array<number>; btm: Array<number> };
 };
 
-const getGame = (N: number) => {
+const getGame = (nParticipants: number, maxRounds: number = 0) => {
   const game: GameType = {};
   const participants: Array<number> = Array.from(
-    { length: N },
+    { length: nParticipants },
     (_, x) => x + 1
   );
   // If there is an odd number of participants, add a placeholder
   // participant '0'. When participants match with '0', they take
   // a break from participating for the round
-  if (N % 2 !== 0) participants.unshift(0);
-  const nTemp: number = participants.length;
-  const nRounds = nTemp - 1;
-  for (let roundNum = 0; roundNum < nRounds; roundNum++) {
-    const top: Array<number> = participants.slice(0, nTemp / 2);
-    const btm: Array<number> = participants.slice(nTemp / 2, nTemp).reverse();
-    game[roundNum] = { top, btm };
-    participants.splice(1, 0, participants[nTemp - 1]);
+  if (nParticipants % 2 !== 0) participants.unshift(0);
+  const N: number = participants.length;
+  const nRounds = maxRounds > N - 1 ? N - 1 : maxRounds;
+  for (let round = 0; round < nRounds; round++) {
+    const top: Array<number> = participants.slice(0, N / 2);
+    const btm: Array<number> = participants.slice(N / 2, N).reverse();
+    game[round] = { top, btm };
+    participants.splice(1, 0, participants[N - 1]);
     participants.pop();
   }
-
   return game;
 };
 
