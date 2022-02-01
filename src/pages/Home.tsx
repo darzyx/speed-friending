6f8c793f71a576bf0e1486c9bea0335e50cce697
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-31 21:20:07 -0500

    Add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Speed Vibing</h1>
+      <h2>Create New Session</h2>
+      <h2>Ongoing Sessions</h2>
+    </div>
+  );
+};
+
+export default Home;
