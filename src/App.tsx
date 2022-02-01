99352eb07e95c0772e3484e07ceec0ae7f28c34f
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-31 21:59:24 -0500

    Clean up page size, margin, padding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Session from "./pages/session/Session";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <Session />
+    <div className="App">
+      <Home />
     </div>
   );
 };
