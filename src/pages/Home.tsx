a387b3b28ecaaf822b1002167d48ec7b32e10017
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-31 23:09:39 -0500

    Center home page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
 const Home = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        margin: "0",
+        padding: "0",
+      }}
+    >
       <h1>Speed Vibing</h1>
       <h2>Create New Session</h2>
       <h2>Ongoing Sessions</h2>
