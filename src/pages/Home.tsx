bf73ce7ca5d1a9106e9c740728f690d2e9d1cb7d
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-01 21:44:39 -0500

    Add styled-components package and use for HomeContainer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,21 @@
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+`;
+
 const Home = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "0",
-        padding: "0",
-      }}
-    >
+    <HomeContainer>
       <h1>Speed Vibing</h1>
       <h2>Create New Session</h2>
       <h2>Ongoing Sessions</h2>
-    </div>
+    </HomeContainer>
   );
 };
 
