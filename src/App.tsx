f1d2337ccd62c87ca3830bb7f540341dbc980c80
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-05 17:24:10 -0500

    Query and store sessions in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import { onSnapshot, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+
+import { db } from "./firebase";
 
 import Home from "./pages/Home";
 import Session from "./pages/session/Session";
 
 const App = () => {
+  const [sessions, setSessions] = useState([{}]);
+  console.log({ sessions });
+
+  // Returns onSnapshot because return its value terminates listener
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "sessions"), (snapshot) => {
+        setSessions(snapshot.docs.map((doc) => doc.data()));
+      }),
+    []
+  );
+
   return (
     <div className="App">
       <div className="Main">
