686ed1eb4cb5d94650540ea948fa404a38dc2604
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-06 00:07:50 -0500

    Style home links as segments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { DocumentData } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Segment } from "semantic-ui-react";
 
 const HomeContainer = styled.div`
   display: flex;
@@ -20,9 +21,17 @@ const Home = ({ sessions }: HomePropsType) => {
       <h3>Ongoing Sessions</h3>
       {hasSessions ? (
         sessions.map((session) => (
-          <Link to={`/session/${session.id}`} key={session.id}>
+          <Segment
+            inverted
+            raised
+            as={Link}
+            to={`/session/${session.id}`}
+            key={session.id}
+            style={{ width: "100%", maxWidth: "600px" }}
+            textAlign="center"
+          >
             {session.name}
-          </Link>
+          </Segment>
         ))
       ) : (
         <p>Loading...</p>
