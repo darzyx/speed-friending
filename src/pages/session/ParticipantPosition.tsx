5e7f501a79d6a2dc1dd8f3c1e0d89ef38a88439c
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-07 21:13:38 -0500

    Clean up session participants color scheme

--- src/pages/session/ParticipantPosition.tsx
+++ src/pages/session/ParticipantPosition.tsx
@@ -21,13 +21,18 @@ const ParticipantPosition = ({
       height: "100%",
       minHeight: "50px",
       width: "100%",
-      color: selectedRoundIsActive ? "black" : "white",
+      maxWidth: "100px",
+      color: "white",
       fontWeight: "bold",
       fontSize: "22px",
-      backgroundColor: selectedRoundIsActive ? getColor(n) : "#1b1c1d",
+      backgroundColor: "#1b1c1d",
       border: "2px solid black",
       borderRadius: top ? "8px 8px 0 0" : "0 0 8px 8px",
       boxSizing: "border-box",
+      ...(selectedRoundIsActive && {
+        backgroundColor: getColor(n),
+        color: "black",
+      }),
     }}
   >
     {n}
