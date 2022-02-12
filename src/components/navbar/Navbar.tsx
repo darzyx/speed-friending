2b955141b52b9d0fa4f51941b1d973af478a6a4a
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-12 09:05:29 -0500

    Keep nav buttons bg color fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import { Button, Icon } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import InfoModal from "./InfoModal";
+
+const StyledButton = styled(Button).attrs({ secondary: true })`
+  &&&& {
+    background-color: #1b1c1d !important;
+  }
+`;
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -11,12 +18,12 @@ const Navbar = () => {
 
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Button onClick={() => navigate("/")} secondary>
+      <StyledButton onClick={() => navigate("/")}>
         <Icon name="home" /> Home
-      </Button>
-      <Button onClick={() => setOpenInfoModal(true)} secondary>
+      </StyledButton>
+      <StyledButton onClick={() => setOpenInfoModal(true)}>
         <Icon name="info circle" /> Info
-      </Button>
+      </StyledButton>
       <InfoModal
         openInfoModal={openInfoModal}
         setOpenInfoModal={setOpenInfoModal}
