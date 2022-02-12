61aac2f7cfe8dd641292e05e8fb3760e42338df2
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-11 23:05:06 -0500

    Fix navbar items positioning

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Icon, Menu } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 
 import InfoModal from "./InfoModal";
@@ -10,24 +10,18 @@ const Navbar = () => {
   const [openInfoModal, setOpenInfoModal] = useState(false);
 
   return (
-    <Menu inverted secondary>
-      <Menu.Item>
-        <Button onClick={() => navigate("/")} secondary>
-          <Icon name="home" /> Home
-        </Button>
-      </Menu.Item>
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Button onClick={() => setOpenInfoModal(true)} secondary>
-            <Icon name="info circle" /> Info
-          </Button>
-          <InfoModal
-            openInfoModal={openInfoModal}
-            setOpenInfoModal={setOpenInfoModal}
-          />
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Button onClick={() => navigate("/")} secondary>
+        <Icon name="home" /> Home
+      </Button>
+      <Button onClick={() => setOpenInfoModal(true)} secondary>
+        <Icon name="info circle" /> Info
+      </Button>
+      <InfoModal
+        openInfoModal={openInfoModal}
+        setOpenInfoModal={setOpenInfoModal}
+      />
+    </div>
   );
 };
 
