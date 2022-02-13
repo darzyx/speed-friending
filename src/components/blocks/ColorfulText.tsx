8f3827fdccbc19104072303f001f7c1a1ff59247
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-12 21:15:44 -0500

    Add another color to home page CTA and speed up color animation

--- src/components/blocks/ColorfulText.tsx
+++ src/components/blocks/ColorfulText.tsx
@@ -2,9 +2,10 @@ import { Header } from "semantic-ui-react";
 import styled, { keyframes } from "styled-components";
 
 const colorChange = keyframes`
-  0% { color: #008080; }
-  50% { color: #c25089; }
-  100% { color: #6495ed; }
+  0% { color: #a84677; }
+  25% { color: #008080; }
+  50% { color: #6495ed; }
+  100% { color: #9d4ebd; }
 `;
 
 export const ColorfulHeader = styled(Header).attrs({
@@ -12,7 +13,7 @@ export const ColorfulHeader = styled(Header).attrs({
   textAlign: "center",
 })`
   animation-name: ${colorChange};
-  animation-duration: 5s;
+  animation-duration: 3s;
   animation-iteration-count: infinite;
   animation-direction: alternate;
   animation-timing-function: ease-in-out;
