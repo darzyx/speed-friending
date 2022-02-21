364366c46214127499f2774b27e4e6afda79bb3d
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-21 10:34:03 -0500

    Delete unused file

--- src/utils.tsx
+++
@@ -1,5 +0,0 @@
-// Object.keys returns keys as strings, but for type safety we want
-// to keep the original keys type:
-export const getKeys = Object.keys as <T extends object>(
-  obj: T
-) => Array<keyof T>;
