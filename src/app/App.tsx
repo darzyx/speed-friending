bba5d4c43cba77a7540468e8e18f22c10475ca26
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-27 10:16:31 -0500

    Undo previous change to ready state for audio

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -66,7 +66,7 @@ const App = () => {
 
   const [mute, setMute] = useState(true);
 
-  const [startReady, setStartReady] = useState(!mute);
+  const [startReady, setStartReady] = useState(true);
   const [playStartSfx] = useSound(startSfx);
   const playStartSfxIfUnmute = () => {
     if (!mute && startReady) {
@@ -77,7 +77,7 @@ const App = () => {
     }
   };
 
-  const [finishReady, setFinishReady] = useState(!mute);
+  const [finishReady, setFinishReady] = useState(true);
   const [playFinishSfx] = useSound(finishSfx);
   const playFinishSfxIfUnmute = () => {
     if (!mute && finishReady) {
