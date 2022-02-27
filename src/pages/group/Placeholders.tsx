dacbb7777ca25dd4f03dbeeb6d33145c46297e0e
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-27 13:24:20 -0500

    Update copy

--- src/pages/group/Placeholders.tsx
+++ src/pages/group/Placeholders.tsx
@@ -7,7 +7,7 @@ export const GroupNotFound = () => (
   <CenterMiddle style={{ minHeight: "50vh" }}>
     <Icon name="window close outline" size="massive" />
     <Divider hidden />
-    <p>This group does not exist. It may have been deleted</p>
+    <p>This group doesn't exist. It may have been deleted</p>
     <ColorfulLink to="/home">
       Find existing groups on the home page &rarr;
     </ColorfulLink>
