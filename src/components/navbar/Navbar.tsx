0666e5bd6d75a1936bf8882a237e4878579afdaf
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-02-27 05:55:54 -0500

    Add test audio button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useSound from "use-sound";
 
 import InfoModal from "./InfoModal";
 import NavButton from "../blocks/NavButton";
 import StyledIcon from "../blocks/StyledIcon";
 import SettingsModal from "./SettingsModal";
+
+// @ts-ignore Cannot find module or its corresponding type declarations.ts(2307)
+import alarmSfx from "../../media/alarm.ogg";
 
 const Navbar = ({
   inverted,
@@ -19,14 +23,21 @@ const Navbar = ({
 }) => {
   const navigate = useNavigate();
 
+  const [play] = useSound(alarmSfx);
+
   const [openSettingsModal, setOpenSettingsModal] = useState(false);
   const [openInfoModal, setOpenInfoModal] = useState(false);
 
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <NavButton onClick={() => navigate("/home")}>
-        <StyledIcon name="home" />
-      </NavButton>
+      <div>
+        <NavButton onClick={() => navigate("/home")} margin="0 10px 0 0">
+          <StyledIcon name="home" />
+        </NavButton>
+        <NavButton onClick={play}>
+          <StyledIcon name="unmute" />
+        </NavButton>
+      </div>
       <div>
         <NavButton
           margin="0 10px 0 0"
